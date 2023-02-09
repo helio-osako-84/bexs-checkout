32a829cf9fc28e19d834c6bb70509a6878141eaa
root <root@HELIO-DESKTOP>
2023-02-09 14:17:39 -0200

    removendo uso de prop especifica para onchange no componente InputText

--- src/components/input-text/index.js
+++ src/components/input-text/index.js
@@ -4,46 +4,33 @@ import style from './style.module.css';
 const InputText = ({
   label,
   id,
-  onInputChange,
   errorMessage,
   isInError,
   ...props
-}) => {
-  const inputProps = {
-    id,
-    name: id,
-    type: 'text',
-    placeholder: label,
-    ...props
-  };
-
-  if (onInputChange) {
-    inputProps.onChange = onInputChange;
-  }
-
-  return (
-    <div className={isInError ? style.isInError : ''}>
-      <label
-        htmlFor={id}
-        className={style.label}
-      >
-        {label}
-      </label>
-      <input 
-        className={style.input} 
-        {...inputProps}
-      />
-      <span className={style.errorMessage}>
-        {errorMessage}
-      </span>
-    </div>
-  );
-}
+}) => (
+  <div className={isInError ? style.isInError : ''}>
+    <label
+      htmlFor={id}
+      className={style.label}
+    >
+      {label}
+    </label>
+    <input 
+      name={id}
+      type="text"
+      className={style.input} 
+      placeholder={label}
+      {...props}
+    />
+    <span className={style.errorMessage}>
+      {errorMessage}
+    </span>
+  </div>
+);
 
 InputText.propTypes = {
   label: PropTypes.string,
   id: PropTypes.string,
-  onInputChange: PropTypes.func,
   errorMessage: PropTypes.string,
   isInError: PropTypes.bool
 }
